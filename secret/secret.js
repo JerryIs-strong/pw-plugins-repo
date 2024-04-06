8bdb1f9d94df0b7fccb4a7b6e699183972df3a80
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2024-04-07 01:40:18 +0800

    Add files via upload

--- secret/secret.js
+++ secret/secret.js
@@ -1,1 +1,18 @@
-
+const titleElement = document.getElementById('title');
+let clickCount = 0;
+let loveCount = 0;
+let aboveLove = 0;
+styleInfo("Welcome to", "secret plugins", "#91b185", "#fff");
+titleElement.addEventListener("click", function () {
+  clickCount++;
+  if (clickCount === 10) {
+    titleElement.innerText = "";
+    clickCount = 0;
+    loveCount += 1;
+    if (loveCount > 0 && loveCount <= 3) {
+      titleElement.innerText += '❤'.repeat(loveCount);
+    } else {
+      titleElement.innerText += "💘x" + (aboveLove += 1);
+    }
+  }
+});
