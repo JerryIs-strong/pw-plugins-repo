d9eae2189a0f132d7b91d1e9afbbdb1d90525ce7
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2024-04-24 17:42:27 +0800

    Update secret.js

--- secret/secret.js
+++ secret/secret.js
@@ -1,18 +1,18 @@
 const titleElement = document.getElementById('title');
 let clickCount = 0;
-let loveCount = 0;
-let aboveLove = 0;
+let ObjectCount = 0;
+let overflow = 0;
 styleInfo("Welcome to", "secret plugins", "#91b185", "#fff");
 titleElement.addEventListener("click", function () {
   clickCount++;
   if (clickCount === 10) {
     titleElement.innerText = "";
     clickCount = 0;
-    loveCount += 1;
-    if (loveCount > 0 && loveCount <= 3) {
-      titleElement.innerText += '❤'.repeat(loveCount);
+    ObjectCount += 1;
+    if (ObjectCount > 0 && ObjectCount <= 3) {
+      titleElement.innerText += '❤'.repeat(ObjectCount);
     } else {
-      titleElement.innerText += "💘x" + (aboveLove += 1);
+      titleElement.innerText += "💘x" + (overflow += 1);
     }
   }
 });
