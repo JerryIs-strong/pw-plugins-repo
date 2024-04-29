7650875904d4a4a185aac37b5ae6247217fa351d
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2024-04-29 22:43:31 +0800

    Add files via upload

--- aplayer-arona/aplayer-arona.js
+++ aplayer-arona/aplayer-arona.js
@@ -39,16 +39,37 @@ function config() {
         }]
     });
 
+    const hash = JSON.stringify({
+        track: md5(`/src/music/${music.name}/track.mp3`),
+        cover: md5(`/src/music/${music.name}/cover.png`)
+    })
+    console.log(JSON.parse(hash));
+
+    fetch(`/src/music/${music.name}/highligh.txt`)
+        .then(res => {
+            if (!res.ok) {
+                debug("highligh.txt not exist", "info");
+                return;
+            }
+            return res.text();
+        })
+        .then(data => {
+            if (data) {
+                showSnackbar(`${data} -「${music.name}」`, true, 8000, "#867fc1", "fa-solid", "fa-heart");
+            }
+        })
+        .catch(error => {
+            console.error('Error occurred while fetching the file:', error);
+        });
+
     ap.on('loadeddata', function () {
-        showSnackbar("aplayer-arona 加載完成!", false, 6000);
+        showSnackbar("aplayer-arona 加載完成!", false, 2000);
     });
 
     ap.on('error', function () {
-        showSnackbar("aplayer-arona 發生錯誤!", false, 6000, "#d55757", "fa-solid", "fa-bomb");
+        showSnackbar("aplayer-arona 發生錯誤!", false, 2000, "#d55757", "fa-solid", "fa-bomb");
     });
 }
-
-
 
 addDepend();
 addElement();
