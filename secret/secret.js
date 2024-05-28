7116aab2ad6f32731eb3f59c8bc6a417f7ca9da5
Heyajerry <96361414+JerryIs-strong@users.noreply.github.com>
2024-05-28 21:22:14 +0800

    Update secret.js

--- secret/secret.js
+++ secret/secret.js
@@ -1,4 +1,5 @@
 const titleElement = document.getElementById('title');
+const message = "";
 let clickCount = 0;
 let ObjectCount = 0;
 let overflow = 0;
@@ -6,7 +7,7 @@ styleInfo("Welcome to", "secret plugins", "#91b185", "#fff");
 titleElement.addEventListener("click", function () {
   clickCount++;
   if (clickCount === 10) {
-    titleElement.innerText = "";
+    titleElement.innerText = message;
     clickCount = 0;
     ObjectCount += 1;
     if (ObjectCount > 0 && ObjectCount <= 3) {
